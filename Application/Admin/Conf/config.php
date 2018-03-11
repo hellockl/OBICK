320506c5a53c6b8a6da9722b423f3100648a43fe
ckliang <350271335@qq.com>
2018-03-11 22:43:25 +0800

    add

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -11,5 +11,6 @@ return array(
     'DB_PREFIX'             =>  'n_',    // 数据库表前缀 
     'DB_CHARSET'            =>  'utf8',      // 数据库编码 
     'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
-    'HTTP_UPLOAD'    =>  __ROOT__.'/Public/upload/'
+    'HTTP_UPLOAD'    =>  __ROOT__.'/Public/upload/',
+    '__PUBLIC__'    =>  __ROOT__.'/Public/'
 );
---
+++ Index/Controller/IndexController.class.php
@@ -0,0 +1,29 @@
+<?php
+namespace Index\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+        
+        $this->display();
+    }
+    
+    public function exhibition(){
+        $this->display();
+    }
+    
+    public function experience(){
+        $this->display();
+    }
+    
+    public function goods(){
+        $this->display();
+    }
+    
+    public function guest(){
+        $this->display();
+    }
+    
+    public function information(){
+        $this->display();
+    }
+}
